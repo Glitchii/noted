13b00354df363cf6ca87dfabfd0f8b7eb7e7956b
Glitchii <darlionthesis@gmail.com>
2023-03-08 21:10:06 +0000

    Remove custom toolbar, update themes, temporary update to ManageActivity

--- app/src/main/java/com/example/noted/ManageActivity.java
+++ app/src/main/java/com/example/noted/ManageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.StatusBarManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -17,6 +18,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ManageActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,22 +38,50 @@ public class ManageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
-        getSupportActionBar().hide();
 
         String username;
         Intent intent = getIntent();
-        TextView greetingName = findViewById(R.id.greetingName);
+        ActionBar actionBar = getSupportActionBar();
 
         // Get username from intent shared from LoginActivity
         username = intent.getStringExtra("username");
         // Capitalize the first letter of the username
         username = username.substring(0, 1).toUpperCase() + username.substring(1);
 
-        greetingName.setText(username);
-        ActionBar actionBar = getSupportActionBar();
-
-
-        actionBar.setTitle("Hello, " + username);
+        actionBar.setTitle(username);
         Toast.makeText(this, "Logged in as " + username, Toast.LENGTH_LONG).show();
+
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("lol.txt", Context.MODE_PRIVATE);
+            fileOutputStream.write("Hello".getBytes());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Get current timestamp
+        // https://strftime.org | https://stackoverflow.com/a/23068721/11848657
+        Log.d("DEBUG_TAG", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+
+        // Create 'notes' folder where all note files will be created
+        File notesDir = new File(getFilesDir(), "notes");
+        if (!notesDir.exists())
+            notesDir.mkdir();
+
+        // Loop through the 'notes' directory to find any files to be displayed in list
+        File[] files = notesDir.listFiles();
+        for (File file : files)
+            Log.d("DEBUG_TAG", file.getAbsolutePath());
+
+        // Create new file with current time-stamp
+        // String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        // String filename = username + "-" + timestamp;
+        // try {
+        //     if (new File(notesDir, filename).createNewFile())
+        //         Log.d("DEBUG_TAG", "File " + filename + " created.");
+        // } catch (IOException e) {
+        //     throw new RuntimeException(e);
+        // }
     }
 }
