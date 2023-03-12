9c6d2b3841db5a688dc8bdad05b467a7f4dffea1
Glitchii <darlionthesis@gmail.com>
2023-03-12 17:19:58 +0000

    Complete 'background switcher' page, add root id to root layouts.

--- app/src/main/java/com/example/noted/BackgroundSwitcherActivity.java
+++ app/src/main/java/com/example/noted/BackgroundSwitcherActivity.java
@@ -3,10 +3,18 @@ package com.example.noted;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BackgroundSwitcherActivity extends GlobalAppCompatActivity {
+    private TextView resetButton;
+    private boolean resetButtonShown = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +22,89 @@ public class BackgroundSwitcherActivity extends GlobalAppCompatActivity {
         super.configure(R.layout.activity_background_switcher);
 
         EditText hexBox = findViewById(R.id.hexBox);
+        LinearLayout suggestionsBox = findViewById(R.id.suggestionsBox);
+        resetButton = findViewById(R.id.resetButton);
 
         // Focus into hex box and show keyboard after 200ms (https://stackoverflow.com/a/7784904/11848657)
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                // Focus into input field. Might not show keyboard
-                hexBox.requestFocus();
-                // Additional code to show the keyboard
-                hexBox.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0f, 0f, 0));
-                hexBox.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, 0f, 0f, 0));
+        new Handler().postDelayed(() -> {
+            // Focus into input field. Might not show keyboard
+            hexBox.requestFocus();
+            // Additional code to show the keyboard
+            hexBox.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0f, 0f, 0));
+            hexBox.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, 0f, 0f, 0));
+
+
+        }, 2500);
+
+        new Handler().postDelayed(() -> Toast.makeText(this, "Colour persists after app restarts", Toast.LENGTH_SHORT).show(), 200);
+
+        if (chosenBackground != null)
+            setResetButtonAlpha(1f);
+
+        hexBox.addTextChangedListener(new TextWatcher() {
+            // https://developer.android.com/reference/android/text/TextWatcher
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
-        }, 200);
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = s.toString();
+                if (text.length() > 0 && !text.startsWith("#")) {
+                    hexBox.setText("#" + text);
+                    hexBox.setSelection(hexBox.getText().length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                if (text.matches("#[0-9a-fA-F]{6}")) {
+                    saveBackground(text);
+                }
+            }
+        });
+
+        for (int i = 0; i < suggestionsBox.getChildCount(); i++) {
+            View view = suggestionsBox.getChildAt(i);
+
+            view.setOnClickListener(l -> {
+                if (view.getId() == R.id.cs1)
+                    saveBackground("#131714");
+                else if (view.getId() == R.id.cs2)
+                    saveBackground("#191211");
+                else if (view.getId() == R.id.cs3)
+                    saveBackground("#171721");
+                else if (view.getId() == R.id.cs4)
+                    saveBackground("#181406");
+            });
+        }
+    }
+
+    protected void saveBackground(String hex) {
+        super.setBackground(hex);
+
+        // Save colour to be used in other activities and when app is restarted
+        if (hex != defaultBackground)
+            saveCofig("background", hex);
+        else
+            deleteConfig("background");
+
+        // Show reset button
+        if (!resetButtonShown) {
+            // Fade in animation (https://stackoverflow.com/a/36660424/11848657)
+            setResetButtonAlpha(1f);
+            resetButtonShown = true;
+        }
+
+        resetButton.setOnClickListener(v -> {
+            saveBackground(defaultBackground);
+            resetButtonShown = false;
+            setResetButtonAlpha(0f);
+            deleteConfig("background");
+        });
+    }
+
+    private void setResetButtonAlpha(float v) {
+        resetButton.animate().alpha(v).setDuration(500).start();
     }
 }
--- app/src/main/java/com/example/noted/GlobalAppCompatActivity.java
+++ app/src/main/java/com/example/noted/GlobalAppCompatActivity.java
@@ -2,12 +2,16 @@ package com.example.noted;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,8 +23,8 @@ import java.io.FileOutputStream;
  * ...
  */
 public class GlobalAppCompatActivity extends AppCompatActivity {
-    protected String username;
     protected Intent intent;
+    protected String username, chosenBackground, defaultBackground = "#1B1A1D";
 
     /**
      * Sets action bar text and background colour.
@@ -48,72 +52,83 @@ public class GlobalAppCompatActivity extends AppCompatActivity {
 
         if (username != null)
             saveLogin(username);
-        else
-            checkLogin();
+
+        checkConfigs();
 
         // Capitalise the first letter of the username
-        Log.d("DEBUG_USERNAME", username);
         username = username.substring(0, 1).toUpperCase() + username.substring(1);
         // Configure action bar
         actionBarConfig(this, username);
-
-        Log.d("DEBUG_CREATED", "Activity created");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        checkLogin();
+        checkConfigs();
+
+        if (chosenBackground == null)
+            setBackground(defaultBackground);
     }
 
-    /**
-     * Checks whether any user is still logged in.
-     * Useful when starting an activity that requires a login.
-     * Starts login activity if no one is logged in.
-     */
-    private void checkLogin() {
+    private void checkConfigs() {
         try {
-            username = getLoggedInUser();
+            // Check whether any user is still logged in.
+            username = getConfigValue("loggedInUser");
             if (username == null)
+                // Starts login activity if no one is logged in.
                 toLoginActivity();
         } catch (Exception e) {
             Log.d("DEBUG_LOGIN_SAVE", e.toString());
             Toast.makeText(this, "Failed finding who is logged in", Toast.LENGTH_SHORT).show();
             toLoginActivity();
         }
-    }
-
-    public void saveLogin(String username) {
-        // Could use Shared-Preferences (https://developer.android.com/training/data-storage/shared-preferences)
-        // Instead of creating a custom file but no need for a small project like this
-        // since this also works.
 
         try {
-            FileOutputStream fos = openFileOutput(".loggedInUser", Context.MODE_PRIVATE);
-            fos.write(username.getBytes());
-            fos.close();
+            // Check whether a custom background was set colour
+            chosenBackground = getConfigValue("background"); // #RRGGBB
+            if (chosenBackground != null)
+                setBackground(chosenBackground);
         } catch (Exception e) {
-            Log.d("DEBUG_LOGIN", e.toString());
-            Toast.makeText(this, "Failed saving login.", Toast.LENGTH_LONG).show();
+            Snackbar.make(findViewById(android.R.id.content), e.toString(), Snackbar.LENGTH_LONG);
+            Toast.makeText(this, chosenBackground, Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void logout() {
-        File file = new File(getFilesDir(), ".loggedInUser");
-        if (file.exists())
-            file.delete();
+    public void saveLogin(String username) {
+        saveCofig("loggedInUser", username);
+    }
 
+    public boolean saveCofig(String key, String value) {
+        // Could use Shared-Preferences (https://developer.android.com/training/data-storage/shared-preferences)
+        // Instead of creating a custom file but no need for a small project like this since this also works.
+
+        try {
+            FileOutputStream fos = openFileOutput('.' + key, Context.MODE_PRIVATE);
+            fos.write(value.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            Log.d("DEBUG_LOGIN", e.toString());
+            Toast.makeText(this, "Failed saving info.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void logout() {
+        deleteConfig("loggedInUser");
         toLoginActivity();
     }
 
-    public String getLoggedInUser() throws Exception {
-        File file = new File(getFilesDir(), ".loggedInUser");
-        return file.exists() ? Utils.readFile(file) : null;
+    public void deleteConfig(String key) {
+        File file = new File(getFilesDir(), '.' + key);
+        if (file.exists())
+            file.delete();
     }
 
-    public boolean loggedInAs(String username) throws Exception {
-        String content = getLoggedInUser();
-        return content != null && content.equals(username);
+    public String getConfigValue(String key) throws Exception {
+        File file = new File(getFilesDir(), '.' + key);
+        return file.exists() ? Utils.readFile(file) : null;
     }
 
     private void toLoginActivity() {
@@ -122,5 +137,22 @@ public class GlobalAppCompatActivity extends AppCompatActivity {
         //developer.android.com/reference/android/content/Intent#constants_1
         loginIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(loginIntent);
+    }
+
+    protected void setBackground(String hex) {
+        View root = findViewById(R.id.root);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor(hex));
+
+        if (root == null)
+            return;
+
+        // Change background colour
+        root.setBackground(colorDrawable);
+        // Change action bar background colour
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+        // Change status bar background colour
+        getWindow().setStatusBarColor(Color.parseColor(hex));
+        // Change navigation bar background colour
+        getWindow().setNavigationBarColor(Color.parseColor(hex));
     }
 }
