ea5badb60bfe7707257d82d5defb579646e1ffed
Glitchii <darlionthesis@gmail.com>
2023-03-17 16:09:33 +0000

    Basic quiz activity setup

---
+++ app/src/main/java/com/example/noted/Quiz.java
@@ -0,0 +1,12 @@
+package com.example.noted;
+
+import android.os.Bundle;
+
+public class Quiz extends GlobalAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.configure(R.layout.activity_quiz);
+    }
+}
